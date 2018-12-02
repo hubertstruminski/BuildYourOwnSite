26490cde9508f909611e97d51f194d44feeaa4f9
HUbert Strumiński <hubert.struminski@gmail.com>
2018-12-02 23:20:16 +0100

    add regular expressions to project, validation function, disabled button by default and subsite showing message after click on button to send email

--- javascript/kontakt.js
+++ javascript/kontakt.js
@@ -1,5 +1,58 @@
+var btnSend = document.getElementById("btn");
+var name = document.getElementById("name");
+var surname = document.getElementById("surname");
+var email = document.getElementById("email");
+var txtArea = document.getElementById("txtarea");
+
+function validateForm()
+{
+	var regName = /^[A-Z]{1}[a-zA-Z ]{2,}/;
+	var regSpace = /[ ]{1,}/;
+	var regEmail = /^[a-z0-9]{1,}@[a-z0-9]{1,}\.[a-z0-9]{1,}/;
+
+	if(name.value == "" || surname.value == "" || email.value == "" || txtArea.value == "" ||
+		name.value == "Wpisz swoje imię" || surname.value == "Wpisz swoje nazwisko" || email.value == "Podaj swój email" || txtArea.value == "Czym jesteś zainteresowany?"){
+		btnSend.disabled = true;
+	}else if(!regName.test(name.value) || regSpace.test(name.value)){
+		name.style.border = "2px solid red";
+		btnSend.disabled = true;
+	}else if(!regEmail.test(email.value) || regSpace.test(email.value)){
+		email.style.border = "2px solid red";
+		btnSend.disabled = true;
+	}else{
+		btnSend.disabled = false;
+	}
+}
+
+function switchButton()
+{
+	if(name.value != "" && email.value != "" && txtArea.value != ""){
+		btnSend.disabled = false;
+		btnSend.style.backgroundColor = "#c43a3a";
+	}
+}
+
+function init()
+{
+	name.value = "";
+	surname.value = "";
+	email.value = "";
+	txtArea.value = "";
+}
+
+function enableButton()
+{
+	btnSend.disabled = true;
+	btnSend.style.backgroundColor = "#666";
+}
+
 window.onload = function()
 {
+	init();
+
+	enableButton();
+	switchButton();
+
 	var ul = document.querySelector("ul");
 
 	var firstLi = document.getElementsByTagName("li")[0];
@@ -23,6 +76,7 @@ window.onload = function()
 	ul.appendChild(fourthLink);
 
 
+
 	firstLi.addEventListener("click", function(){
 		window.location = firstLink.getAttribute("href");
 	});
@@ -37,5 +91,11 @@ window.onload = function()
 
 	fourthLi.addEventListener("click", function(){
 		window.location = fourthLink.getAttribute("href");
+	});
+
+
+
+	btnSend.addEventListener("click", function(){
+		validateForm();
 	});
 }
