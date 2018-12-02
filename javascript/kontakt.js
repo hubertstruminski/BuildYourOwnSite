fc132da96031b771d68b38ed554e781104c8780e
HUbert Strumiński <hubert.struminski@gmail.com>
2018-12-02 20:50:38 +0100

    check whether files with extension html valid correct - done!

--- javascript/kontakt.js
+++ javascript/kontakt.js
@@ -1,18 +1,3 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
-} 
-
 window.onload = function()
 {
 	var ul = document.querySelector("ul");
@@ -53,6 +38,4 @@ window.onload = function()
 	fourthLi.addEventListener("click", function(){
 		window.location = fourthLink.getAttribute("href");
 	});
-
-	showSlides();
 }
